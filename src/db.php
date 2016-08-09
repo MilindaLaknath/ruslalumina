52ad3e7fd1fa96d551ec7493ed4a23013ede7106
milinda <milinda@dhcppc2>
2016-08-09 23:14:35 +0530

    sql dump

--- src/db.php
+++ src/db.php
@@ -1,7 +1,7 @@
 <?php
 
 // 1. Create a database connection
-$conn = mysqli_connect("127.0.0.1", "root", "IronMan@123", "alumna");
+$conn = mysqli_connect("127.0.0.1", "root", "123", "alumna");
 // Test if connection succeeded
 if (mysqli_connect_errno()) {
     die("Database connection failed: " . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")"
